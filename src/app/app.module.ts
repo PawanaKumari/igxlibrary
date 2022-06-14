20fc238b7f646fd9330de8ed7df7cab0025de1d2
seasia.developers2 <seasia.developers2@gmail.com>
2022-06-14 16:09:09 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IgxGeographicMapModule } from "igniteui-angular-maps";
 
 @NgModule({
   declarations: [
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FormsModule,
+    IgxGeographicMapModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
